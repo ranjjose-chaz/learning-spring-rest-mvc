adadfe671be1431518131428709c8e3f6418cf3c
Ranjith.Chaz <Ranjith.Chaz@CanadaLife.de>
2024-02-13 17:50:13 +0000

    Add basic exception logic by defining and wiring @ExceptionHandler annotation

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -124,6 +124,15 @@ class BeerControllerTest {
     }
 
     @Test
+    void getBeerByIdNotFound() throws Exception {
+
+        given(beerService.getBeerById(any(UUID.class))).willThrow(new NotFoundException());
+
+        mockMvc.perform(get(BEER_PATH_ID, UUID.randomUUID()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     void deleteBeerById() throws Exception {
         Beer beerToBeDeleted = beerServiceImpl.listBeers().get(0);
         mockMvc.perform(delete(BEER_PATH_ID, beerToBeDeleted.getId().toString())
