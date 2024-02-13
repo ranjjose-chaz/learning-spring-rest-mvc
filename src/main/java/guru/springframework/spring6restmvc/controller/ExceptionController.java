7fa80a4a1fd02b20df1744c4825e2d82120f699f
Ranjith.Chaz <Ranjith.Chaz@CanadaLife.de>
2024-02-13 18:18:56 +0000

    Add @ResponseStatus annotation and simplify

--- src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
@@ -4,11 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+/*@ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity handleNotFoundException() {
         return ResponseEntity.notFound().build();
     }
 
-}
+}*/
